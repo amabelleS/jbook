16d255cc80584eb186eb6982d41736d754da769d
amabelle <amabelletr@gmail.com>
2021-04-14 19:18:19 +0300

    transpiling works:)

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,47 @@
-import * as esbuild from 'esbuild-wasm'
-import {useState, useEffect, useRef} from 'react'
-import ReactDOM from 'react-dom'
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
-    const ref = useRef<any>()
-    const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuils.wasm'
-        })
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    useEffect(() => {
-        startService()
-    }, [])
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
 
-    const onClick = () => {
-        if (!ref.current) {
-            return;
-        }
+    setCode(result.code);
+ };
 
-        console.log(ref.current);
-    }
-
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <pre>{code}</pre>
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-}
+  );
+};
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
