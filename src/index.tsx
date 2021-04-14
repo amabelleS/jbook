a30686966f5c3016aad63d952cf67eb6a6142453
amabelle <amabelletr@gmail.com>
2021-04-14 05:42:51 +0300

    initializing ESBuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
-import {useState} from 'react'
+import * as esbuild from 'esbuild-wasm'
+import {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
     const [input, setInput] = useState('')
     const [code, setCode] = useState('')
 
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuils.wasm'
+        })
+
+        console.log(service);
+    }
+
+    useEffect(() => {
+        startService()
+    }, [])
+    
     const onClick = () => {
         console.log(input);
     }
