cb0acd6e8557e430ff4edac22c306f48fc794ea4
amabelle <amabelletr@gmail.com>
2021-04-15 05:42:46 +0300

    generating the unpkg url

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,11 +9,17 @@ export const unpkgPathPlugin = () => {
         console.log('onResole', args);
         if (args.path === 'index.js') {
             return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg')
+        } 
         return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
             namespace: 'a',
+            path: `https://unpkg.com/${args.path}`
         }
+        
+        // else if (args.path === 'tiny-test-pkg')
+        // return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        // }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
