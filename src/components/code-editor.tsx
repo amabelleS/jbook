1021d4f16efc71bc5465c882395a4170e81533cd
amabelle <amabelletr@gmail.com>
2021-04-20 00:17:55 +0300

    adding more type defs

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,14 @@
 import MonacoEditor from '@monaco-editor/react'
 
 const codeEditor = () => {
-    return <MonacoEditor height="220px" language="javascript"/>
+    return <MonacoEditor 
+    height="220px" 
+    language="javascript" 
+    theme="dark"
+    options={{
+        wordWrap: 'on',
+    }}
+    />
 }
 
 export default codeEditor
