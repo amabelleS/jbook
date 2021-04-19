1a8758fc6eef3f5d5621a0864e39930c499b806a
amabelle <amabelletr@gmail.com>
2021-04-19 23:17:42 +0300

    highlighting errors

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,13 @@ const App = () => {
     <div id="root"></div>
     <script>
     window.addEventListener('message', event => {
-      eval(event.data)
+      try {
+         eval(event.data)
+      } catch (err) {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red; background-color: yellow; padding: 0.5rem"><h4>Runtime Error</h4>' + err + '</div>'
+      }
+     
     }, false)
     </script>
     </body>
