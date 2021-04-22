a2d8a5aa6822167da8df7bf736768517106a2b36
amabelle <amabelletr@gmail.com>
2021-04-23 02:57:31 +0300

    fixing the resize bug

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css'
 import {useEffect, useRef} from 'react';
 import { iterate } from "localforage"
 
@@ -34,7 +35,16 @@ const Preview: React.FC<PreviewProps> = ({code}) => {
         iframe.current.contentWindow.postMessage(code, '*')
     }, [code])
 
-    return <iframe style={{backgroundColor: 'white'}} title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>;
+       return (
+        <div className="preview-wrapper">
+        <iframe 
+          title="preview" 
+          ref={iframe} 
+          sandbox="allow-scripts" 
+          srcDoc={html}
+        />
+        </div>
+      );
 }
 
 export default Preview
