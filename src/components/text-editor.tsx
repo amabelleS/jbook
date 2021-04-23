bb1fb99918b4ed06838dd9ed461dcac761846ab2
amabelle <amabelletr@gmail.com>
2021-04-23 18:24:07 +0300

    toggling the markdown editor mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+    const [editing, setEditing] = useState(false);
     const [value, setValue] = useState("**Hello world!!!**");
-    return <div>
-        <MDEditor
-        value={value}
-        // onChange={setValue}
-      />
-      <MDEditor.Markdown source={value} />
+
+    useEffect(() => {
+        const listner = () => {
+            setEditing(false)
+        }
+        document.addEventListener('click', listner, {capture: true})
+
+        return () => {
+            document.removeEventListener('click', listner, {capture: true})
+        }
+    }, [])
+
+    if (editing) {
+        return <div>
+            <MDEditor value={value}/>
+        </div>
+    }
+    return <div onClick={() => setEditing(true)}>
+        <MDEditor.Markdown source={value} />
     </div>
 }
 
