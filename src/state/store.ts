80e31d14d1bd904d889ef770c97b155f11b725d2
amabelle <amabelletr@gmail.com>
2021-04-24 02:35:19 +0300

    Returnig state, immer gotcha

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,7 @@
+import { stat } from 'fs';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
