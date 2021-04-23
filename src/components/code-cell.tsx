6615513e384971312c8e55d8a89f8e2545924a8c
amabelle <amabelletr@gmail.com>
2021-04-23 03:42:59 +0300

    resizing horizontally

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,12 +18,13 @@ const CodeCell = () => {
   return (
       <Resizable direction='vertical'>
     <div style={{height: '100%', display: 'flex', flexDirection: 'row'}}>
+      <Resizable direction='horizontal'>
       <CodeEditor 
       initialValue="const a = 1;"
       onChange= {(value) => {
         setInput(value)
       }} />
-     
+     </Resizable>
       <Preview code={code}/>
     </div>
     </Resizable>
