a22ec5ed74b9fdbae2abe406136ac24f84b078e4
amabelle <amabelletr@gmail.com>
2021-04-23 04:11:19 +0300

    getting preview to expand

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -13,6 +13,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
   if (direction === 'horizontal') {
     resizableProps = {
+    className: 'resize-horizontal',
     minConstraints:[window.innerWidth * 0.2, Infinity],
     maxConstraints: [window.innerWidth * 0.75, Infinity],
     height: Infinity, 
