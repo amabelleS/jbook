c32d8debbd49987a2e183b91988504618a5766b9
amabelle <amabelletr@gmail.com>
2021-04-24 13:55:13 +0300

    initial reducer

--- src/state/reducers/bundelsReducers.ts
+++ src/state/reducers/bundelsReducers.ts
@@ -1,1 +1,30 @@
-export default 1;
+import produce from 'immer';
+import { ActionType } from '../action-types'
+import { Action } from '../actions'
+
+interface BundelsState {
+    [key: string]: {
+        loading: boolean,
+        code: string, 
+        err: string
+    }
+}
+
+const initialState: BundelsState = {}
+
+const reducer = produce((state: BundelsState = initialState, action: Action): BundelsState => {
+    switch (action.type) {
+        case ActionType.BUNDEL_START:
+            
+            return state;
+        case ActionType.BUNDEL_COMPLETE:
+            
+            return state;
+    
+        default:
+            return state;
+    }
+})
+
+
+export default reducer
