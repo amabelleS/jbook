e772473587f740200d447315fbf1bde725d460a9
amabelle <amabelletr@gmail.com>
2021-04-24 17:59:53 +0300

    Fixing the background

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -45,15 +45,19 @@ const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
         updateCell(cell.id, value)
       }} />
      </Resizable>
+     <div className='progress-wrapper'>
      {
-       !bundle  || bundle.loading
-        ? <div className='progress-cover'>
+      !bundle  || bundle.loading
+        ?  (
+          <div className='progress-cover'>
             <progress className='progress is-small is-primary' max='100'>
               Loading...
             </progress>
         </div>
+        )
         : <Preview err={bundle.err} code={bundle.code}/>
      }
+     </div>
       
     </div>
     </Resizable>
