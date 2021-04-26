593492075f2127c91f14993e83ecc7bd8db484c8
amabelle <amabelletr@gmail.com>
2021-04-26 20:43:59 +0300

    pre publish to npm, my-js-snippets

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,26 +1,33 @@
-import {Fragment} from 'react'
-import {useTypedSelector} from '../hooks/use-typed-selector'
-import CellListItem from './cell-list-item'
-import AddCell from './add-cell'
-import './cell-list.css'
+import './cell-list.css';
+import { Fragment, useEffect } from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+import { useActions } from '../hooks/use-actions';
 
 const CellList: React.FC = () => {
-    const cells = useTypedSelector(({cells: {order, data}}) => 
-        order.map((id) => 
-            data[id]
-        )
-    )
-    const renderedCells = cells.map(cell => 
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, [fetchCells]);
+
+  const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
-    <CellListItem  cell={cell}/>
-    <AddCell prevCellId={cell.id}/>
+      <CellListItem cell={cell} />
+      <AddCell prevCellId={cell.id} />
     </Fragment>
-    )
+  ));
 
-    return <div className='cell-list'>
-        <AddCell forceVisible={cells.length === 0} prevCellId={null}/>
-        {renderedCells}
-        </div>
-}
+  return (
+    <div className="cell-list">
+      <AddCell forceVisible={cells.length === 0} prevCellId={null} />
+      {renderedCells}
+    </div>
+  );
+};
 
-export default CellList
+export default CellList;
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from 'redux'
+import axios from 'axios'
 import { ActionType } from '../action-types';
 import {
   Action, 
@@ -9,8 +10,9 @@ import {
   InsertCellAfterAction,
 } from '../actions';
 
-import {CellTypes} from '../cell'
+import { Cell, CellTypes } from '../cell'
 import bundle from '../../bundler'
+import { RootState } from '../reducers'
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
     return {
@@ -68,5 +70,39 @@ export const createBundle = (cellId: string, input: string) => {
         bundle: result,
       },
     });
+  };
+};
+
+export const fetchCells = () => {
+
+  return async (dispach: Dispatch<Action>) => {
+    dispach({ type: ActionType.FETCH_CELLS})
+
+    try {
+      const { data }: { data: Cell[]} = await axios.get('/cells')
+
+      dispach({ type: ActionType.FETCH_CELLS_COMPLETE, payload: data})
+    } catch (err) {
+      dispach({ type: ActionType.FETCH_CELLS_ERROR, payload: err.message})
+    }
+  };
+}
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+
+    const cells = order.map((id) => data[id]);
+
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err) {
+      dispatch({
+        type: ActionType.SAVE_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
   };
 };
